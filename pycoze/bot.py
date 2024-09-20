2b99179fe988ab0045eb7d17b034f4315f3133ca
chyroc <chyroc@qq.com>
2024-09-20 15:15:28 +0800

    update comment

--- pycoze/bot.py
+++ pycoze/bot.py
@@ -1,26 +1,83 @@
+from enum import IntEnum
+from typing import List
+
 from pycoze.auth import Auth
 from pycoze.model import CozeModel
 from pycoze.request import Requester
 
 
 class BotPromptInfo(CozeModel):
+    # Configured prompt of the bot.
     prompt: str
 
 
+class BotOnboardingInfo(CozeModel):
+    # Configured prologue of the bot.
+    prologue: str = ''
+    # The list of recommended questions configured for the bot.
+    # This field will not be returned when user suggested questions are not enabled.
+    suggested_questions: List[str] = []
+
+
 class BotModelInfo(CozeModel):
+    # The ID of the model.
     model_id: str
+    # The name of the model.
     model_name: str
 
 
+class BotMode(IntEnum):
+    SingleAgent = 0
+    MultiAgent = 1
+    SingleAgentWorkflow = 2
+
+
+class BotPluginAPIInfo(CozeModel):
+    # The ID of the tool.
+    api_id: str
+    # The name of the tool.
+    name: str
+    # The description of the tool.
+    description: str
+
+
+class BotPluginInfo(CozeModel):
+    # The unique identifier for the plugin.
+    plugin_id: str
+    # The name of the plugin.
+    name: str
+    # The description of the plugin.
+    description: str
+    # The avatar URL for the plugin.
+    icon_url: str
+    # The list of tool information for the plugin. For more information, see PluginAPI object.
+    api_info_list: List[BotPluginAPIInfo]
+
+
 class Bot(CozeModel):
-    bot_id: str  # Bot 的唯一标识
-    name: str  # Bot 的名称
-    description: str  # Bot 的描述信息
-    icon_url: str  # Bot 的图标
-    create_time: int  # 创建时间，格式为 10 位的 Unixtime 时间戳，单位为秒（s）
-    update_time: int  # Bot 的更新时间
-    version: str  # Bot 最新版本的版本号
+    # The ID for the bot.
+    bot_id: str
+    # The name of the bot.
+    name: str
+    # The description of the bot.
+    description: str
+    # The URL address for the bot's avatar.
+    icon_url: str
+    # The creation time, in the format of a 10-digit Unix timestamp in seconds (s).
+    create_time: int
+    # The update time, in the format of a 10-digit Unix timestamp in seconds (s).
+    update_time: int
+    # The latest version of the bot.
+    version: str
+    # The prompt configuration for the bot. For more information, see Prompt object.
     prompt_info: BotPromptInfo
+    # The onboarding message configuration for the bot. For more information, see Onboarding object.
+    onboarding_info: BotOnboardingInfo
+    # The mode of the Bot, values: 0: Single Agent mode, 1: Multi Agent mode, 3: Single Agent Workflow mode.
+    bot_mode: BotMode
+    # The plugins configured for the bot. For more information, see  Plugin object.
+    plugin_info_list: List[BotPluginInfo]
+    # The model configured for the bot. For more information, see Model object.
     model_info: BotModelInfo
 
 
@@ -36,9 +93,11 @@ class BotClient(object):
 
     def get_online_info_v1(self, *, bot_id: str):
         """
+        Get the configuration information of the bot, which must have been published
+        to the Bot as API channel.
+        docs: https://www.coze.com/docs/developer_guides/get_metadata?_lang=en
+
         :calls: `GET /v1/bot/get_online_info`
-        :param bot_id:
-        :return:
         """
         url = f'{self._base_url}/v1/bot/get_online_info'
         params = {
