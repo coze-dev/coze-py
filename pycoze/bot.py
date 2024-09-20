e8179ccf8b5c8be69838a54ab363c04398259048
chyroc <chyroc@qq.com>
2024-09-20 15:34:05 +0800

    feat: add bot.get_online_info_v1 feature (#1)
    * feat: add bot.get_online_info_v1 feature
    
    * update comment

---
+++ pycoze/bot.py
@@ -0,0 +1,107 @@
+from enum import IntEnum
+from typing import List
+
+from pycoze.auth import Auth
+from pycoze.model import CozeModel
+from pycoze.request import Requester
+
+
+class BotPromptInfo(CozeModel):
+    # Configured prompt of the bot.
+    prompt: str
+
+
+class BotOnboardingInfo(CozeModel):
+    # Configured prologue of the bot.
+    prologue: str = ''
+    # The list of recommended questions configured for the bot.
+    # This field will not be returned when user suggested questions are not enabled.
+    suggested_questions: List[str] = []
+
+
+class BotModelInfo(CozeModel):
+    # The ID of the model.
+    model_id: str
+    # The name of the model.
+    model_name: str
+
+
+class BotMode(IntEnum):
+    SingleAgent = 0
+    MultiAgent = 1
+    SingleAgentWorkflow = 2
+
+
+class BotPluginAPIInfo(CozeModel):
+    # The ID of the tool.
+    api_id: str
+    # The name of the tool.
+    name: str
+    # The description of the tool.
+    description: str
+
+
+class BotPluginInfo(CozeModel):
+    # The unique identifier for the plugin.
+    plugin_id: str
+    # The name of the plugin.
+    name: str
+    # The description of the plugin.
+    description: str
+    # The avatar URL for the plugin.
+    icon_url: str
+    # The list of tool information for the plugin. For more information, see PluginAPI object.
+    api_info_list: List[BotPluginAPIInfo]
+
+
+class Bot(CozeModel):
+    # The ID for the bot.
+    bot_id: str
+    # The name of the bot.
+    name: str
+    # The description of the bot.
+    description: str
+    # The URL address for the bot's avatar.
+    icon_url: str
+    # The creation time, in the format of a 10-digit Unix timestamp in seconds (s).
+    create_time: int
+    # The update time, in the format of a 10-digit Unix timestamp in seconds (s).
+    update_time: int
+    # The latest version of the bot.
+    version: str
+    # The prompt configuration for the bot. For more information, see Prompt object.
+    prompt_info: BotPromptInfo
+    # The onboarding message configuration for the bot. For more information, see Onboarding object.
+    onboarding_info: BotOnboardingInfo
+    # The mode of the Bot, values: 0: Single Agent mode, 1: Multi Agent mode, 3: Single Agent Workflow mode.
+    bot_mode: BotMode
+    # The plugins configured for the bot. For more information, see  Plugin object.
+    plugin_info_list: List[BotPluginInfo]
+    # The model configured for the bot. For more information, see Model object.
+    model_info: BotModelInfo
+
+
+class BotClient(object):
+    """
+    Bot class.
+    """
+
+    def __init__(self, base_url: str, auth: Auth, requester: Requester):
+        self._base_url = base_url
+        self._auth = auth
+        self._requester = requester
+
+    def get_online_info_v1(self, *, bot_id: str):
+        """
+        Get the configuration information of the bot, which must have been published
+        to the Bot as API channel.
+        docs: https://www.coze.com/docs/developer_guides/get_metadata?_lang=en
+
+        :calls: `GET /v1/bot/get_online_info`
+        """
+        url = f'{self._base_url}/v1/bot/get_online_info'
+        params = {
+            'bot_id': bot_id
+        }
+
+        return self._requester.request('get', url, Bot, params=params)
--- pycoze/request.py
+++ pycoze/request.py
@@ -1,7 +1,22 @@
 from typing import TYPE_CHECKING, Optional
 
+import requests
+
 if TYPE_CHECKING:
     from pycoze.auth import Auth
+
+from typing import Type, TypeVar
+
+from pydantic import BaseModel
+
+T = TypeVar('T', bound=BaseModel)
+
+
+def json_obj_to_pydantic(json_obj: dict, model: Type[T]) -> T:
+    """
+    将 JSON 字符串转换为指定的 Pydantic 模型对象。
+    """
+    return model.model_validate(json_obj)
 
 
 class Requester(object):
@@ -14,11 +29,32 @@ class Requester(object):
                  ):
         self._auth = auth
 
-    def request(self, method: str, path: str, **kwargs) -> dict:
+    def request(self, method: str, url: str, model: Type[T], params: dict = None, headers: dict = None) -> dict:
         """
         Send a request to the server.
         """
-        pass
+        if headers is None:
+            headers = {}
+        self._auth.authentication(headers)
+        r = requests.request(method, url, params=params, headers=headers)
+        logid = r.headers.get('x-tt-logid')
+
+        try:
+            json = r.json()
+            code = json.get('code') or 0
+            msg = json.get('msg') or ''
+            data = json.get('data')
+        except:
+            r.raise_for_status()
+
+            code = 0
+            msg = ''
+            data = {}
+
+        if code > 0:
+            # TODO: Exception 自定义类型
+            raise Exception(f'{code}: {msg}, logid:{logid}')
+        return model.model_validate(data)
 
     async def arequest(self, method: str, path: str, **kwargs) -> dict:
         """
