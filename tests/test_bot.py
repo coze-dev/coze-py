e29e6a753e7c1fc8566eaa1fb37633c5f9591a72
chyroc <chyroc@qq.com>
2024-09-22 14:43:25 +0800

    chore: add test ci (#3)

---
+++ tests/test_bot.py
@@ -0,0 +1,19 @@
+import os
+from unittest import TestCase
+
+from cozepy import PersonalAccessToken, Coze
+
+
+class TestBotClient(TestCase):
+    def test_list_published_bots_v1(self):
+        space_id = os.getenv('SPACE_ID_1').strip()
+        token = os.getenv('COZE_TOKEN').strip()
+        for i in token:
+            print('token', i)
+        auth = PersonalAccessToken(token)
+        cli = Coze(auth=auth, base_url='https://api.coze.cn')
+
+        res = cli.bot.list_published_bots_v1(space_id=space_id, page_size=2)
+        assert res.total > 1
+        assert res.has_more
+        assert len(res.items) > 1
