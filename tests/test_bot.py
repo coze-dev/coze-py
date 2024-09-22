f59013d0a5dd8fad77842ced5e58ee39b60c3554
chyroc <chyroc@qq.com>
2024-09-22 09:09:12 +0800

    fix ci

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -6,8 +6,8 @@ from cozepy import PersonalAccessToken, Coze
 
 class TestBotClient(TestCase):
     def test_list_published_bots_v1(self):
-        space_id = os.getenv('SPACE_ID_1')
-        auth = PersonalAccessToken(os.getenv('PYPI_TOKEN'))
+        space_id = os.getenv('SPACE_ID_1').strip()
+        auth = PersonalAccessToken(os.getenv('PYPI_TOKEN').strip())
         cli = Coze(auth=auth)
 
         res = cli.bot.list_published_bots_v1(space_id=space_id, page_size=2)
