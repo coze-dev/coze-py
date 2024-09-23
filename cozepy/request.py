a155601632955dd75fb16a9761ce7f9b29c85ee3
chyroc <chyroc@qq.com>
2024-09-23 16:01:32 +0800

    feat: get oauth token by jwt flow (#4)

--- cozepy/request.py
+++ cozepy/request.py
@@ -1,6 +1,7 @@
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Tuple, Optional
 
 import requests
+from requests import Response
 
 if TYPE_CHECKING:
     from cozepy.auth import Auth
@@ -25,35 +26,30 @@ class Requester(object):
     """
 
     def __init__(self,
-                 auth: Optional["Auth"]
+                 auth: 'Auth' = None
                  ):
         self._auth = auth
 
-    def request(self, method: str, url: str, model: Type[T], params: dict = None, headers: dict = None) -> T:
+    def request(self, method: str, url: str, model: Type[T], params: dict = None, headers: dict = None,
+                body: dict = None, ) -> T:
         """
         Send a request to the server.
         """
         if headers is None:
             headers = {}
-        self._auth.authentication(headers)
-        r = requests.request(method, url, params=params, headers=headers)
-        logid = r.headers.get('x-tt-logid')
+        if self._auth:
+            self._auth.authentication(headers)
+        r = requests.request(method, url, params=params, headers=headers, json=body)
 
-        try:
-            json = r.json()
-            code = json.get('code') or 0
-            msg = json.get('msg') or ''
-            data = json.get('data')
-        except:
-            r.raise_for_status()
+        code, msg, data = self.__parse_requests_code_msg(r)
 
-            code = 0
-            msg = ''
-            data = {}
-
-        if code > 0:
+        if code is not None and code > 0:
             # TODO: Exception 自定义类型
+            logid = r.headers.get('x-tt-logid')
             raise Exception(f'{code}: {msg}, logid:{logid}')
+        elif code is None and msg != "":
+            logid = r.headers.get('x-tt-logid')
+            raise Exception(f'{msg}, logid:{logid}')
         return model.model_validate(data)
 
     async def arequest(self, method: str, path: str, **kwargs) -> dict:
@@ -61,3 +57,18 @@ class Requester(object):
         Send a request to the server with asyncio.
         """
         pass
+
+    def __parse_requests_code_msg(self, r: Response) -> Tuple[Optional[int], str, Optional[T]]:
+        try:
+            json = r.json()
+        except:
+            r.raise_for_status()
+            return
+
+        if 'code' in json and 'msg' in json and int(json['code']) > 0:
+            return int(json['code']), json['msg'], json['data']
+        if 'error_message' in json and json['error_message'] != '':
+            return None, json['error_message'], None
+        if 'data' in json:
+            return 0, '', json['data']
+        return 0, '', json
