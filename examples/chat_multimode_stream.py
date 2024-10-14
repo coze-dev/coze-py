8aa2934ccaba74bbd5fd5439796e7ae56d538ff6
chyroc <chyroc@qq.com>
2024-10-14 16:05:43 +0800

    fix: Resolve dependency issue with httpx in higher Python versions (#74)
    - Fixed compatibility issues with httpx in higher Python versions

--- examples/chat_multimode_stream.py
+++ examples/chat_multimode_stream.py
@@ -4,17 +4,21 @@ with image upload and handle chat events
 """
 
 import os  # noqa
+import sys
+
+from cozepy import COZE_COM_BASE_URL
 
 # Get an access_token through pat or oauth.
 api_coze_token = os.getenv("COZE_API_TOKEN")
+api_base = os.getenv("COZE_API_BASE") or COZE_COM_BASE_URL
 
-from cozepy import Coze, TokenAuth, Message, ChatStatus, MessageContentType, ChatEventType, MessageObjectString  # noqa
+from cozepy import Coze, TokenAuth, Message, ChatEventType, MessageObjectString  # noqa
 
 # Init the Coze client through the access_token.
-coze = Coze(auth=TokenAuth(token=api_coze_token))
+coze = Coze(auth=TokenAuth(token=api_coze_token), base_url=api_base)
 
 # Create a bot instance in Coze, copy the last number from the web link as the bot's ID.
-bot_id = "bot id"
+bot_id = os.getenv("COZE_BOT_ID")
 # The user id identifies the identity of a user. Developers can use a custom business ID
 # or a random string.
 user_id = "user id"
@@ -23,7 +27,8 @@ from pathlib import Path  # noqa
 
 # Call the upload interface to upload a picture requiring text recognition, and
 # obtain the file_id of the picture.
-file = coze.files.upload(file=Path("/path/image.jpg"))
+file_path = sys.argv[1] if len(sys.argv) > 1 else "/path/image.jpg"
+file = coze.files.upload(file=Path(file_path))
 
 # Call the coze.chat.stream method to create a chat. The create method is a streaming
 # chat and will return a Chat Iterator. Developers should iterate the iterator to get
@@ -42,4 +47,4 @@ for event in coze.chat.stream(
 ):
     if event.event == ChatEventType.CONVERSATION_MESSAGE_DELTA:
         message = event.message
-        print(f"role={message.role}, content={message.content}")
+        print(event.message.content, end="")
