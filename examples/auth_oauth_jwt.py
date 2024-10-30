c3045e0d13ff1f4b61a1af25ca11d4c03ddcdf49
Kasperunas, I. <39649532+miyukiko-su@users.noreply.github.com>
2024-10-30 16:11:17 +0900

    update: Add private key file reader example and removed obsolete ttl value in auth_oauth_jwt.py

--- examples/auth_oauth_jwt.py
+++ examples/auth_oauth_jwt.py
@@ -26,6 +26,12 @@ jwt_oauth_private_key = os.getenv("COZE_JWT_OAUTH_PRIVATE_KEY")
 # public key id
 jwt_oauth_public_key_id = os.getenv("COZE_JWT_OAUTH_PUBLIC_KEY_ID")
 
+# If the private key is stored separately as a file with .pem extension, 
+# you may read its content properly and then use it as private key here in code
+jwt_oauth_private_key_file_path = "./private_key.pem"  # replace with actual path
+with open(jwt_oauth_private_key_file_path, "rb") as private_key_file:
+    jwt_oauth_private_key = private_key_file.read().decode("utf-8")
+
 # The sdk offers the JWTOAuthApp class to establish an authorization for Service OAuth.
 # Firstly, it is required to initialize the JWTOAuthApp.
 
@@ -45,7 +51,7 @@ jwt_oauth_app = JWTOAuthApp(
 # the jwt oauth process.
 
 # Generate the authorization token
-oauth_token = jwt_oauth_app.get_access_token(ttl=3600)
+oauth_token = jwt_oauth_app.get_access_token()
 
 # use the access token to init Coze client
 coze = Coze(auth=TokenAuth(oauth_token.access_token), base_url=coze_api_base)
