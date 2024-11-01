3b90ba446d523198be849e91b868642562612956
Kasperunas, I. <39649532+miyukiko-su@users.noreply.github.com>
2024-11-01 11:27:39 +0900

    docs: Update JWT OAuth usage example (#104)
    - Changed the name of the variable that stores the path to the file with
    the private key
    - Updated the variable description in the comment above to clarify its
    meaning and purpose

--- examples/auth_oauth_jwt.py
+++ examples/auth_oauth_jwt.py
@@ -23,13 +23,13 @@ coze_api_base = os.getenv("COZE_API_BASE") or COZE_COM_BASE_URL
 jwt_oauth_client_id = os.getenv("COZE_JWT_OAUTH_CLIENT_ID")
 # private key
 jwt_oauth_private_key = os.getenv("COZE_JWT_OAUTH_PRIVATE_KEY")
-# The private key file that stores the private key with .pem extension
-jwt_oauth_private_file = os.getenv("COZE_JWT_OAUTH_PRIVATE_FILE")
+# path to the private key file (usually with .pem extension)
+jwt_oauth_private_key_file_path = os.getenv("COZE_JWT_OAUTH_PRIVATE_KEY_FILE_PATH")
 # public key id
 jwt_oauth_public_key_id = os.getenv("COZE_JWT_OAUTH_PUBLIC_KEY_ID")
 
-if jwt_oauth_private_file:
-    with open(jwt_oauth_private_file, "r") as f:
+if jwt_oauth_private_key_file_path:
+    with open(jwt_oauth_private_key_file_path, "r") as f:
         jwt_oauth_private_key = f.read()
 
 
