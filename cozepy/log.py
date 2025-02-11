7311cecd4e5400ce863b2624d2ffae99d3bcb751
Lexiay <lexia.liu@keeson.com>
2025-02-11 17:03:15 +0800

    fix: avoid adding duplicate handlers

--- cozepy/log.py
+++ cozepy/log.py
@@ -2,6 +2,12 @@ import logging
 
 logger = logging.getLogger("cozepy")
 
+console_handler = logging.StreamHandler()
+
+formatter = logging.Formatter("[cozepy][%(levelname)s][%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+console_handler.setFormatter(formatter)
+logger.propagate = False
+
 
 def setup_logging(level: int = logging.WARNING) -> None:
     if level not in [
@@ -14,14 +20,9 @@ def setup_logging(level: int = logging.WARNING) -> None:
     ]:
         raise ValueError(f"invalid log level: {level}")
 
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(level)
-
-    formatter = logging.Formatter("[%(name)s][%(levelname)s][%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
-    console_handler.setFormatter(formatter)
-
-    logger.addHandler(console_handler)
     logger.setLevel(level)
+    if not logger.handlers:
+        logger.addHandler(console_handler)
 
 
 log_fatal = logger.fatal
