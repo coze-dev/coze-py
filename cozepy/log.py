877cf565a1d0990c48a37bbc0bfd44e088b3756e
chyroc <chyroc@qq.com>
2025-02-19 11:14:34 +0800

    Update cozepy/log.py

--- cozepy/log.py
+++ cozepy/log.py
@@ -7,7 +7,7 @@ console_handler = logging.StreamHandler()
 formatter = logging.Formatter("[cozepy][%(levelname)s][%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 console_handler.setFormatter(formatter)
 logger.propagate = False
-
+logger.addHandler(console_handler)
 
 def setup_logging(level: int = logging.WARNING) -> None:
     if level not in [
