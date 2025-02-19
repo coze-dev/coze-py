3017372667330f22a4b314380e90fe93a9b9a549
chyroc <chyroc@qq.com>
2025-02-19 11:15:50 +0800

    Update cozepy/log.py

--- cozepy/log.py
+++ cozepy/log.py
@@ -3,7 +3,6 @@ import logging
 logger = logging.getLogger("cozepy")
 
 console_handler = logging.StreamHandler()
-
 formatter = logging.Formatter("[cozepy][%(levelname)s][%(asctime)s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 console_handler.setFormatter(formatter)
 logger.propagate = False
