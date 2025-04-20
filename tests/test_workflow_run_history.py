4117ac7a15f2a5d7c32cd80c344175b6568b1a0e
chyroc <chyroc@qq.com>
2025-04-20 17:20:16 +0800

    Fix parsing of WorkflowRunHistory in running state

---
+++ tests/test_workflow_run_history.py
@@ -0,0 +1,22 @@
+from cozepy import WorkflowRunHistory
+
+
+def test_empty_str_to_zero():
+    data = {
+        "logid": "logid",
+        "debug_url": "https://www.coze.cn/work_flow",
+        "execute_id": "123",
+        "bot_id": "0",
+        "update_time": 1744960910,
+        "token": "0",
+        "execute_status": "Running",
+        "connector_uid": "123",
+        "connector_id": "1024",
+        "create_time": 1744960910,
+        "run_mode": 2,
+        "output": '{"Output":"null"}',
+        "cost": "0.00000",
+        "error_code": "",
+    }
+    history = WorkflowRunHistory(**data)
+    print("history", history)
