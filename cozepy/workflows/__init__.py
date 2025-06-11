e560d74798beb5b7900cf64ccb2c60e08bd1206f
chyroc <chyroc@qq.com>
2025-06-11 16:30:56 +0800

    workflow-list

--- cozepy/workflows/__init__.py
+++ cozepy/workflows/__init__.py
@@ -1,11 +1,48 @@
-from typing import TYPE_CHECKING, Optional
+from enum import Enum
+from typing import TYPE_CHECKING, List, Optional
 
+from cozepy import NumberPaged, NumberPagedResponse
+from cozepy.model import AsyncNumberPaged, CozeModel, HTTPRequest
 from cozepy.request import Requester
-from cozepy.util import remove_url_trailing_slash
+from cozepy.util import remove_none_values, remove_url_trailing_slash
 
 if TYPE_CHECKING:
     from .chat import AsyncWorkflowsChatClient, WorkflowsChatClient
     from .runs import AsyncWorkflowsRunsClient, WorkflowsRunsClient
+
+
+class WorkflowMode(str, Enum):
+    WORKFLOW = "workflow"
+    CHATFLOW = "chatflow"
+
+
+class PublishStatus(str, Enum):
+    ALL = "all"  # 所有智能体，且数据为最新草稿版本
+    PUBLISHED_ONLINE = "published_online"  # 已发布智能体的最新线上版本
+    PUBLISHED_DRAFT = "published_draft"  # 已发布的最新草稿版本
+    UNPUBLISHED_DRAFT = "unpublished_draft"  # 未发布的最新草稿版本
+
+
+class Workflow(CozeModel):
+    workflow_id: str
+    workflow_name: str
+    description: str
+    icon_url: str
+    app_id: str
+
+
+class _PrivateListWorkflowData(CozeModel, NumberPagedResponse[Workflow]):
+    items: List[Workflow]
+    has_more: bool
+
+    def get_total(self) -> Optional[int]:
+        return None
+
+    def get_has_more(self) -> Optional[bool]:
+        return self.has_more
+
+    def get_items(self) -> List[Workflow]:
+        return self.items
 
 
 class WorkflowsClient(object):
@@ -31,6 +68,43 @@ class WorkflowsClient(object):
             self._chat = WorkflowsChatClient(self._base_url, self._requester)
         return self._chat
 
+    def list(
+        self,
+        *,
+        workspace_id: Optional[str] = None,
+        workflow_mode: Optional[WorkflowMode] = None,
+        app_id: Optional[str] = None,
+        publish_status: Optional[PublishStatus] = None,
+        page_num: int = 1,
+        page_size: int = 100,
+    ) -> NumberPaged[Workflow]:
+        url = f"{self._base_url}/v1/workflows"
+
+        def request_maker(i_page_num: int, i_page_size: int) -> HTTPRequest:
+            return self._requester.make_request(
+                "GET",
+                url,
+                params=remove_none_values(
+                    {
+                        "workspace_id": workspace_id,
+                        "workflow_mode": workflow_mode,
+                        "app_id": app_id,
+                        "publish_status": publish_status,
+                        "page_num": i_page_num,
+                        "page_size": i_page_size,
+                    }
+                ),
+                cast=_PrivateListWorkflowData,
+                stream=False,
+            )
+
+        return NumberPaged(
+            page_num=page_num,
+            page_size=page_size,
+            requestor=self._requester,
+            request_maker=request_maker,
+        )
+
 
 class AsyncWorkflowsClient(object):
     def __init__(self, base_url: str, requester: Requester):
@@ -54,3 +128,43 @@ class AsyncWorkflowsClient(object):
 
             self._chat = AsyncWorkflowsChatClient(self._base_url, self._requester)
         return self._chat
+
+    async def list(
+        self,
+        *,
+        workspace_id: Optional[str] = None,
+        workflow_mode: Optional[WorkflowMode] = None,
+        app_id: Optional[str] = None,
+        publish_status: Optional[PublishStatus] = None,
+        page_num: int = 1,
+        page_size: int = 100,
+        **kwargs,
+    ) -> AsyncNumberPaged[Workflow]:
+        url = f"{self._base_url}/v1/workflows"
+        headers: Optional[dict] = kwargs.get("headers")
+
+        async def request_maker(i_page_num: int, i_page_size: int) -> HTTPRequest:
+            return await self._requester.amake_request(
+                "GET",
+                url,
+                params=remove_none_values(
+                    {
+                        "workspace_id": workspace_id,
+                        "workflow_mode": workflow_mode,
+                        "app_id": app_id,
+                        "publish_status": publish_status,
+                        "page_num": i_page_num,
+                        "page_size": i_page_size,
+                    }
+                ),
+                headers=headers,
+                cast=_PrivateListWorkflowData,
+                stream=False,
+            )
+
+        return await AsyncNumberPaged.build(
+            page_num=page_num,
+            page_size=page_size,
+            requestor=self._requester,
+            request_maker=request_maker,
+        )
