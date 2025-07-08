76ef2f6609272098e9e8967e70881298115ce13c
chyroc <chyroc@qq.com>
2025-07-08 12:34:24 +0800

    update

--- tests/test_folders.py
+++ tests/test_folders.py
@@ -39,6 +39,26 @@ def mock_list_folders(respx_mock, total_count, page):
     return logid
 
 
+def mock_retrieve_folder(
+    respx_mock,
+) -> SimpleFolder:
+    folder = SimpleFolder(
+        id=random_hex(10),
+        name=random_hex(10),
+        description=random_hex(10),
+        workspace_id=random_hex(10),
+        creator_user_id=random_hex(10),
+        parent_folder_id=random_hex(10),
+    )
+    folder._raw_response = httpx.Response(
+        200,
+        json={"data": folder.model_dump()},
+        headers={logid_key(): random_hex(10)},
+    )
+    respx_mock.get(f"/v1/folders/{folder.id}").mock(folder._raw_response)
+    return folder
+
+
 @pytest.mark.respx(base_url="https://api.coze.com")
 class TestSyncFolders:
     def test_sync_folders_list(self, respx_mock):
@@ -70,6 +90,14 @@ class TestSyncFolders:
             folder = page.items[0]
             assert folder.id == f"id_{total_result}"
         assert total_result == total
+
+    def test_sync_folders_retrieve(self, respx_mock):
+        coze = Coze(auth=TokenAuth(token="token"))
+        folder = mock_retrieve_folder(respx_mock)
+        resp = coze.folders.retrieve(folder_id=folder.id)
+        assert resp
+        assert resp.id == folder.id
+        assert resp.response.logid == folder._raw_response.headers[logid_key()]
 
 
 @pytest.mark.respx(base_url="https://api.coze.com")
@@ -106,3 +134,11 @@ class TestAsyncFolders:
             folder = page.items[0]
             assert folder.id == f"id_{total_result}"
         assert total_result == total
+
+    async def test_async_folders_retrieve(self, respx_mock):
+        coze = AsyncCoze(auth=AsyncTokenAuth(token="token"))
+        folder = mock_retrieve_folder(respx_mock)
+        resp = await coze.folders.retrieve(folder_id=folder.id)
+        assert resp
+        assert resp.id == folder.id
+        assert resp.response.logid == folder._raw_response.headers[logid_key()]
